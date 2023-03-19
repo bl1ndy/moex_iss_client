34b3b4c42b9d8087310c886232c50b515720eabb
bl1ndy <ayoblindstone@gmail.com>
2023-03-19 17:50:09 +0300

    dev: add version and entrypoint module

---
+++ lib/moex_iss_client.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'moex_iss_client/client'
+require_relative 'moex_iss_client/history_validator'
+require_relative 'moex_iss_client/security'
+require_relative 'moex_iss_client/exceptions'
+
+module MoexIssClient
+end
